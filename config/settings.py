9ba592ba5422bef780ab8947d14522434a4135ff
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-12 21:00:15 +0100

    HNGx TrackBackend Task2

--- config/settings.py
+++ config/settings.py
@@ -83,8 +83,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
-}
+# My Settings 
+CORS_ALLOW_ALL_ORIGINS = True
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES': [
+#         'rest_framework.renderers.JSONRenderer',
+#     ],
+# }
